3dd5c3e9346a0f48d189cf7199ab439a4c1ff5f8
Creosino <creosino@gmail.com>
2016-12-27 01:43:39 -0500

    added in using the expect library

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,20 +1,45 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () =>{
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`)
-
-  }
+  //using expect to perform assertions and test easier
+  expect(res).toBe(44).toBeA('number');
+  // if (res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}.`)
+  //
+  // }
 
 });
 
 it('should square a number', () =>{
-  var wrong = utils.square(3);
+  var res = utils.square(3);
 
-  if (wrong !== 9){
-    throw new Error(`Expected 9, but got ${wrong}.`)
-  }
+  expect(res).toBe(9).toBeA('number');
+  });
 
+  //verifying first and last names are set
+  //assert it includes firstName and lastName with proper values
+it('should set firstName and lastName', () =>{
+  var user = {location: 'Philadelphia', age: 25};
+  var res = utils.setName(user, 'Andrew Mead');
+
+  expect(res).toInclude({
+    firstName: 'Andrew',
+    lastName: 'Mead'
+  });
 });
+
+  // it('should expect some values', () => {
+  //   // expect(12).toNotBe(12);
+  //   // expect({name: 'andrew'}).toNotEqual({name: 'Andrew'});
+  //   expect({
+  //     name: 'Andrew',
+  //     age: 25,
+  //     location: 'Philadelphia'
+  //   }).toExclude({
+  //     age:23
+  //   })
+  // });
